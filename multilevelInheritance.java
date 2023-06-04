798882daadc4c639902569b5110f1746059f9931
Anshu Rajpoot <107042248+Anshu-Rajpoot@users.noreply.github.com>
2023-06-04 21:05:02 +0530

    Add files via upload

---
+++ multilevelInheritance.java
@@ -0,0 +1,28 @@
+class Shape{
+    public void area(){
+        System.out.println("Display Area");
+    }
+}
+class Triangle extends Shape{
+    public void area(int l , int b){
+        System.out.println("Area of Triangle");
+        System.out.println(0.5*l*b);
+    }
+}
+    class rectangle extends Triangle{
+        public void area(int l , int b){
+            System.out.println("Area of Rectangle");
+            System.out.println(l*b);
+        }
+    }
+
+public class multilevelInheritance{
+    public static void main(String args[]){
+        Shape s=new Shape();
+        s.area();
+        Triangle t=new Triangle();
+        t.area(2,3);
+        rectangle r=new rectangle();
+        r.area(2,3);
+    }
+}
